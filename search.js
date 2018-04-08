3bb0d826cee34de0ed446a8ea6041268f34b91ee
lan99 <anlindsay99@gmail.com>
2018-04-08 04:33:58 -0400

    Add files via upload

--- search.js
+++ search.js
@@ -32,6 +32,7 @@ function searchSupply(){
   var cat =  document.getElementById("category").value;
   var quantity =  document.getElementById("quantity").value;
   var zip =  document.getElementById("zip").value;
+  console.log(zip);
   var hName =  document.getElementById("hName").value;
   var searchQuery = "";
 
@@ -73,26 +74,25 @@ function searchSupply(){
 }
   else {
 
-    /*if only zip*/
+    /*if only zip*//*
     if (zip != ""){
 
-          db.collection('Hospitals').find(  {
+          db.collection('Hospitals').find({
 
               $and: [
 
                  {'user' : { $ne: user}},
-
-                 {'zip':zip}
+                 {"zip": zip}
                ]
 
             }).limit(2000).execute();
           }
 
   /*if neither Zip nor name*/
-      else {
+    //  else {
 
         docs = db.collection('Hospitals').find( {'user' : { $ne: user}}).limit(2000).execute();
-      }
+    //  }
 
   }
 
@@ -100,8 +100,12 @@ var html = "<table border = '1'>";
 html = html + "<th>Hospital</th> <th> Address </th> <th> Zip Code </th> <th> Phone</th><th>Email</th><th> Product ID</th> <th> Product Name</th><th>Product Brand</th><th>Product Quantity</th>";
 html = html + "<th>Product Category</th><th>Date Updated</th>";
 
+if (docs){
   docs.then(doc => {
+
         doc.map(c => {
+
+          console.log(c.zip);
 
           console.log(c.supplies);
 
@@ -152,6 +156,14 @@ html = html + "<th>Product Category</th><th>Date Updated</th>";
               }
             }
 
+            if (zip != ""){
+
+              if (zip != c.zip){
+
+                return false;
+              }
+            }
+
             exist = 1;
             html += "<tr> <td>" + c.name + "</td> <td>" + c.address + "</td> <td>" + c.zip + "</td><td>" + c.phone+ "</td> <td>" + c.email+ "</td>";
 
@@ -165,14 +177,12 @@ html = html + "<th>Product Category</th><th>Date Updated</th>";
 
             document.getElementById("supplies").innerHTML = html + "</table>";
 
-            console.log(html);
-
         });
       }
     });
 
 });
-
+}
 if (exist == 0){
 
   document.getElementById("supplies").innerHTML = html + "</table>" + "<br>Nothing Found!";
